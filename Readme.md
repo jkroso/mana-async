443941e358ac5ae7061f1ae4adab6d37ae87c8dc
Jake Rosoman <jkroso@gmail.com>
2015-11-21 13:46:56 +1300

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,17 @@
+# mana-result
+
+handles the rendering of future values in all 3 possible states
+
+## Installation
+
+`npm install jkroso/mana-result`
+
+then in your app:
+
+```js
+var mana-result = require('mana-result')
+```
+
+## API
+
+### `mana-result()`
---
+++ index.js
@@ -0,0 +1,23 @@
+import {Component,STATE} from 'mana'
+import assert from 'assert'
+
+export default class State extends Component {
+  constructor(params, children) {
+    super(params, children)
+    this.previousNode = undefined
+    this[STATE] = params.promise.state
+    if (this.state == 'pending')
+      params.promise.listen(() => this.state = 'done',
+                            () => this.state = 'fail')
+  }
+  update(next, dom) {
+    next.previousNode = this.node
+    delete next.previousNode.events.mount
+    return super.update(next, dom)
+  }
+  render(params) {
+    const fn = params[this.state]
+    assert(typeof fn == 'function')
+    return fn(this)
+  }
+}
